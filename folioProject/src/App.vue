ef573f6d0ef6ed0ff7d1f0adb53db5c106b26217
Laury <laurynejosiasdadygmail.com>
2023-11-28 17:57:12 +0100

    fix(front):manage navbar

---
+++ folioProject/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-1/2 items-center justify-center ml-80">
+    <div class="flex bg-slate-300 items-center justify-center space-x-4 py-2 rounded-xl">
+      <a
+        href="#home"
+        class="text-white hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+        >Accueil</a
+      >
+      <a
+        href="#projects"
+        class="text-white hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+        >Projets</a
+      >
+      <a
+        href="#skills"
+        class="text-white hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+        >Compétences</a
+      >
+      <a
+        href="#experience"
+        class="text-white hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+        >Expérience</a
+      >
+      <a
+        href="#education"
+        class="text-white hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+        >Formation</a
+      >
+      <a
+        href="#contact"
+        class="text-white hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+        >Contact</a
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
